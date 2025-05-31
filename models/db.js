e6a12b85caedfea1baf43eeef342238b6157c951
ahmedhamdinooo <ah908784@gmail.com>
2025-05-31 22:35:37 +0300

    update file db.js

--- models/db.js
+++ models/db.js
@@ -2,8 +2,7 @@ require('dotenv').config({ path: 'server.env' });
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+
 })
 .then(() => console.log("✅ Connected to MongoDB"))
 .catch(err => console.error("❌ MongoDB connection error:", err));
